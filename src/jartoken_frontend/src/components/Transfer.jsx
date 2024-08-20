985af3f84c9bd8db7a32c56b3bc6d834ef6147c3
fajartd02 <fajarmh2@gmail.com>
2024-08-20 08:47:55 +0700

    feat: integration with internet identity. working authenticated after deploy icp

--- src/jartoken_frontend/src/components/Transfer.jsx
+++ src/jartoken_frontend/src/components/Transfer.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { canisterId, createActor } from "../../../declarations/jartoken_backend";
+import { Principal } from "@dfinity/principal";
+import { AuthClient } from "@dfinity/auth-client";
 
 function Transfer() {
-  
+
+  const [toId, setId] = useState("");
+  const [toAmount, setAmount] = useState("");
+  const [isDisabled, setDisable] = useState(false);
+  const [feedback, setFeedback] = useState("");
+  const [idHidden, setHidden] = useState(true);
+
   async function handleClick() {
-    
+    setHidden(true);
+    setDisable(true);
+    const toAccount = Principal.fromText(toId);
+    const addAmount = Number(toAmount);
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+
+    const authCannister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+    const result = await authCannister.transferTo(toAccount, addAmount);
+    setFeedback(result);
+    setHidden(false);
+    setDisable(false);
   }
 
   return (
@@ -16,6 +41,8 @@ function Transfer() {
               <input
                 type="text"
                 id="transfer-to-id"
+                value={toId}
+                onChange={(e) => setId(e.target.value)}
               />
             </li>
           </ul>
@@ -27,15 +54,18 @@ function Transfer() {
               <input
                 type="number"
                 id="amount"
+                value={toAmount}
+                onChange={(e) => setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
+          <button id="btn-transfer" onClick={handleClick} disabled={isDisabled}>
             Transfer
           </button>
         </p>
+        <p hidden={idHidden}>{feedback}</p>
       </div>
     </div>
   );
--- src/jartoken_frontend/src/main.jsx
+++ src/jartoken_frontend/src/main.jsx
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import App from "./components/App";
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import { AuthClient } from "@dfinity/auth-client";
 
+const init = async () => {
+
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      }
+    });
+  }
+
+}
+
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  const userPrinciapl = identity._principal.toString();
+  ReactDOM.render(<App loggedInPrincipal={userPrinciapl} />, document.getElementById("root"));
+}
+
+init();
 
 
